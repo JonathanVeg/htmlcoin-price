1d1f6fecd77cdb8106e1b607e293a254ee305200
Jonathan Silva <jonathan.jgs@gmail.com>
2021-09-13 10:20:24 -0300

    adjusting

--- Entry.ts
+++ Entry.ts
@@ -2,11 +2,11 @@ export default class Entry {
   open = false
   exchange = ''
   market = ''
-  ask = ''
-  bid = ''
-  last = ''
-  low = ''
-  high = ''
-  volume = ''
-  volumeQuote = ''
+  ask = 0
+  bid = 0
+  last = 0
+  low = 0
+  high = 0
+  volume = 0
+  volumeQuote = 0
 }
